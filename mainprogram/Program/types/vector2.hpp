991d711b724ed21b6a173cbd36f99497202428a3
2357 <ryoooo2357g@gmail.com>
2022-11-22 18:34:50 +0900

    feat: Vector2 static method `squareNorm` `norm`
    also, added operator

--- mainprogram/Program/types/vector2.hpp
+++ mainprogram/Program/types/vector2.hpp
@@ -1,6 +1,8 @@
 #ifndef TYPE_VECTOR2_HPP
 #define TYPE_VECTOR2_HPP
 
+#include <math.h>
+
 struct Vector2
 {
     float x;
@@ -10,12 +12,17 @@ struct Vector2
     constexpr Vector2(const int _x, const int _y);
     constexpr Vector2 operator+() const;
     constexpr Vector2 operator-() const;
+    constexpr Vector2 operator+(const Vector2& other) const;
+    constexpr Vector2 operator-(const Vector2& other) const;
     constexpr Vector2 operator*(const float s) const;
     constexpr Vector2 operator/(const float s) const;
     inline Vector2& operator+=(const Vector2& other);
     inline Vector2& operator-=(const Vector2& other);
     inline Vector2& operator*=(const float s);
     inline Vector2& operator/=(const float s);
+
+    static constexpr float squareNorm(const Vector2& vec);
+    static constexpr float norm(const Vector2& vec);
 };
 
 constexpr Vector2::Vector2() : x(0), y(0) {}
@@ -26,6 +33,12 @@ constexpr Vector2 Vector2::operator+() const {
 }
 constexpr Vector2 Vector2::operator-() const {
     return {-x, -y};
+}
+constexpr Vector2 Vector2::operator+(const Vector2& other) const {
+    return {x + other.x, y + other.y};
+}
+constexpr Vector2 Vector2::operator-(const Vector2& other) const {
+    return {x - other.x, y - other.y};
 }
 constexpr Vector2 Vector2::operator*(const float s) const {
     return {x * s, y * s};
@@ -52,6 +65,14 @@ inline Vector2& Vector2::operator/=(const float s) {
     x /= s;
     y /= s;
     return *this;
+}
+
+constexpr float Vector2::squareNorm(const Vector2 &vec) {
+    return vec.x * vec.x + vec.y * vec.y;
+}
+
+constexpr float Vector2::norm(const Vector2 &vec) {
+    return sqrt(vec.x * vec.x + vec.y * vec.y);
 }
 
 #endif
