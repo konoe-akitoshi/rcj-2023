40dc59d1b7107552e0e873aae158755f02dc3d2a
2357 <61523777+ryota2357@users.noreply.github.com>
2023-01-04 23:25:17 +0900

    fix: Gyro

--- mainprogram/Program/Program.cpp
+++ mainprogram/Program/Program.cpp
@@ -72,6 +72,7 @@ const component::MotorController MotorController(MotorCh1, MotorCh2, MotorCh3, M
 const component::Dribbler Dribbler(SetupHandler, PIN_DRIBBLER_PWM);
 const component::Kicker Kicker(SetupHandler, PIN_KICKER);
 
+const component::Gyro Gyro(SetupHandler, 115200);
 const component::XBee XBee(SetupHandler, 9600);
 component::OpenMV OpenMV(SetupHandler, 19200);
 
