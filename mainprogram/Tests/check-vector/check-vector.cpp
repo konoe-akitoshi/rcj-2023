6b61ffd969370536f9bb99c09c0bbab7e3b7ecd8
2357 <61523777+ryota2357@users.noreply.github.com>
2023-01-04 22:59:14 +0900

    test: `std::vector` (vector<int> is used in `component::SetupHandler`

---
+++ mainprogram/Tests/check-vector/check-vector.cpp
@@ -0,0 +1,37 @@
+#ifdef PREVENT_SORCING_FROM_ARDUINO
+#ifdef LOCAL_INCLUDE
+#include "../local/arduino_deps.h"
+#else
+#endif
+
+#include <vector>
+#include "utils/serial_reader.hpp"
+
+util::SerialReader Reader;
+std::vector<int> vec;
+
+void setup() {
+    vec.resize(1);
+    vec[0] = 10;
+    Serial.begin(9600);
+}
+
+int cnt = 0;
+int size = 3;
+
+void loop() {
+    if (vec.size() < 3) {
+        vec.push_back(vec.back() + 1);
+    } else if (cnt == 0) {
+        Serial.println("input number...");
+        int t = Reader.ReadInt();
+        vec.push_back(t);
+        size++;
+    }
+    delay(500);
+    Serial.println(vec[cnt]);
+    cnt++;
+    cnt %= size;
+}
+
+#endif
