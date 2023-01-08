659f7556bcc9d46596b8a43c259b0dea9d2fdcb5
2357 <ryoooo2357g@gmail.com>
2023-01-08 14:57:37 +0900

    test: `component::LedLightPCF8574`

---
+++ mainprogram/Tests/test-led_light_pcf8574/test-led_light_pcf8574.ino
@@ -0,0 +1,32 @@
+#include <Adafruit_PCF8574.h>
+
+#include "components/led_light.hpp"
+#include "components/setup_handler.hpp"
+#include "pin.hpp"
+
+constexpr int TargetLedPin1 = pcf8574::EX02;
+constexpr int TargetLedPin2 = pcf8574::EX03;
+Adafruit_PCF8574 PCF8574;
+component::SetupHandler Handler;
+
+const component::LedLightPCF8574 Light1(Handler, PCF8574, TargetLedPin1);
+const component::LedLightPCF8574 Light2(Handler, PCF8574, TargetLedPin2);
+
+void setup() {
+    Serial.begin(9600);
+    const bool pcf8574_status = PCF8574.begin(0x27, &Wire);
+    if (pcf8574_status == false) {
+        Serial.println("ERROR not find PCF8574");
+        while (true);
+    }
+    Handler.Setup();
+}
+
+void loop() {
+    Light1.TernOn();
+    Light2.TernOff();
+    delay(1000);
+    Light1.TernOff();
+    Light2.TernOn();
+    delay(1000);
+}
