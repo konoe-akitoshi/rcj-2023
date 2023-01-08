23d8e3d12e582f4b6ba37a667b6e90841a7b8d17
2357 <ryoooo2357g@gmail.com>
2023-01-08 18:24:48 +0900

    Revert MotorController

--- mainprogram/Program/components/motor_controller.hpp
+++ mainprogram/Program/components/motor_controller.hpp
@@ -1,23 +1,62 @@
-#ifndef COMPONENTS_MOTOR_CONTOROLER2_HPP
-#define COMPONENTS_MOTOR_CONTOROLER2_HPP
+#ifndef COMPONENTS_MOTOR_HPP
+#define COMPONENTS_MOTOR_HPP
 
 #ifdef LOCAL_INCLUDE
-#include "../../local/wire.hpp"
-#else
-#include <Wire.h>
+#include "../../local/arduino_deps.hpp"
 #endif
 
 #include "setup_handler.hpp"
 
 namespace component
 {
+class Motor
+{
+  public:
+    /**
+     * @param pin_forward_brake forward-brake pin number.
+     * @param pin_reverse_brake reverse-brake pin number.
+     * @param pin_pwm PWM pin number.
+     * @param frequency PWM frequency.
+     */
+    explicit Motor(SetupHandler& handler, const int pin_forward_brake, const int pin_reverse_brake, const int pin_pwm, const int frequency);
+
+    /**
+     * Start motor with specified power. If power is negative, motor move reverse.
+     *
+     * @param power [-100, 100]
+     */
+    void Start(const int power) const;
+
+    /**
+     * Brake motor. Set PWM to 0.
+     */
+    void Stop() const;
+
+    /**
+     * Free reverse-brake and forward-brake.
+     */
+    void Free() const;
+
+  private:
+    const int PIN_FORWARD_BRAKE_;
+    const int PIN_REVERSE_BRAKE_;
+    const int PIN_PWM_;
+};
+
 class MotorController
 {
   public:
     /**
-     * @param address the address of the I2C slave for transmission processing
+     * @param motor1 right-top motor. forward-direction: ↖︎
+     * @param motor2 right-bottom motor. forward-direction: ↗︎
+     * @param motor3 left-bottom motor. forward-direction: ↘︎
+     * @param motor4 left-top motor. forward-direction: ↙︎
+     * @code
+     *   ④ ①   ↙︎ ↖︎
+     *   ③ ②   ↘︎ ↗︎
+     * @endcode
      */
-    explicit MotorController(SetupHandler& handler, const int sda, const int scl, const int address);
+    explicit MotorController(const Motor& motor1, const Motor& motor2, const Motor& motor3, const Motor& motor4);
 
     /**
      * Contorol 4 motors.
@@ -41,56 +80,62 @@ class MotorController
     void FreeAll() const;
 
   private:
-    const int ADDRESS_;
-    uint8_t FormatPower_(const int power) const;
-    void Transmit_(const uint8_t* motorPower, const int size) const;
+    const Motor MOTOR1_, MOTOR2_, MOTOR3_, MOTOR4_;
 };
 
-inline MotorController::MotorController(SetupHandler& handler, const int sda, const int scl, const int address) : ADDRESS_(address) {
-    handler.SetCallback([=] {
-        Wire1.setSDA(sda);
-        Wire1.setSCL(scl);
-        Wire1.begin();
+// Impl for Motor
+
+inline Motor::Motor(SetupHandler& handler, const int pin_forward_brake, const int pin_reverse_brake, const int pin_pwm, const int frequency)
+    : PIN_FORWARD_BRAKE_(pin_forward_brake), PIN_REVERSE_BRAKE_(pin_reverse_brake), PIN_PWM_(pin_pwm) {
+    handler.SetPinMode(PIN_FORWARD_BRAKE_, OUTPUT);
+    handler.SetPinMode(PIN_REVERSE_BRAKE_, OUTPUT);
+    handler.SetPinMode(PIN_PWM_, OUTPUT);
+    handler.SetCallback([=](){
+        analogWriteFrequency(PIN_PWM_, frequency);
     });
 }
 
-inline uint8_t MotorController::FormatPower_(const int power) const {
-    const int p = min(abs(power), 100);
-
-    // bit 8: モーターの回転方向 (0: 正転, 1: 逆転)
-    // bit 1~7: モーターの出力(%)
-    // 0b10000000: ショートブレーキ
-    // 0b00000000: フリーストップ
-    uint8_t ret;
-
-    if (power == 0) {
-        ret = 0b10000000;
-    } else if (power > 0) {
-        ret = p;
-    } else {
-        ret = (p | 0b10000000);
+inline void Motor::Start(const int power) const {
+    if (power == 0) {  // brake
+        Stop();
+        return;
     }
-    return ret;
+    if (power > 0) {  // move forward
+        digitalWrite(PIN_FORWARD_BRAKE_, LOW);
+        digitalWrite(PIN_REVERSE_BRAKE_, HIGH);
+    } else {  // move backward
+        digitalWrite(PIN_FORWARD_BRAKE_, HIGH);
+        digitalWrite(PIN_REVERSE_BRAKE_, LOW);
+    }
+    // power -> 0 < p(power) < 250
+    int p = min(abs(power), 100);
+    p = (p << 1) + (p >> 1);  // p *= 2.5;
+    analogWrite(PIN_PWM_, p);
 }
 
-inline void MotorController::Transmit_(const uint8_t* motorPower, const int size) const {
-    Wire1.beginTransmission(ADDRESS_);
-    Wire1.write(motorPower, size);
-    Wire1.endTransmission();
+inline void Motor::Stop() const {
+    analogWrite(PIN_PWM_, 0);
+    digitalWrite(PIN_FORWARD_BRAKE_, HIGH);
+    digitalWrite(PIN_REVERSE_BRAKE_, HIGH);
+}
+
+inline void Motor::Free() const {
+    digitalWrite(PIN_PWM_, HIGH);
+    digitalWrite(PIN_FORWARD_BRAKE_, LOW);
+    digitalWrite(PIN_REVERSE_BRAKE_, LOW);
+}
+
+// Impl for MotorContoroler
+
+inline MotorController::MotorController(const Motor& motor1, const Motor& motor2, const Motor& motor3, const Motor& motor4)
+    : MOTOR1_(motor1), MOTOR2_(motor2), MOTOR3_(motor3), MOTOR4_(motor4) {
+    MOTOR1_.Stop();
+    MOTOR2_.Stop();
+    MOTOR3_.Stop();
+    MOTOR4_.Stop();
 }
 
 inline void MotorController::Drive(float azimuth, int power, int rotation) const {
-    // それぞれのモーターは次のように配置されている
-    // 各モーターのパワーの比率を x[0] ~ x[3] に計算する
-    //  motor1 right-top motor. forward-direction: ↖︎
-    //  motor2 right-bottom motor. forward-direction: ↗︎
-    //  motor3 left-bottom motor. forward-direction: ↘︎
-    //  motor4 left-top motor. forward-direction: ↙︎
-    // --
-    //   ④ ①   ↙︎ ↖︎
-    //   ③ ②   ↘︎ ↗︎
-    // --
-
     // clamp(power|rotation, -100, 100)
     power = power > 100 ? 100 : power < -100 ? -100 : power;
     rotation = rotation > 100 ? 100 : rotation < -100 ? -100 : rotation;
@@ -121,29 +166,28 @@ inline void MotorController::Drive(float azimuth, int power, int rotation) const
         x[i] /= x_max;
     }
 
-    // NOTE: 受け取り側が6で受け取るようになってるので、配列のサイズは6にすること
-    uint8_t motorPower[6] = {0, 0, 0, 0, 0, 0};
-    for (int i = 0; i < 4; ++i) {
-        motorPower[i] = FormatPower_(x[i] * power);
-    }
-    Transmit_(motorPower, 6);
+    MOTOR1_.Start(x[0] * power);
+    MOTOR2_.Start(x[1] * power);
+    MOTOR3_.Start(x[2] * power);
+    MOTOR4_.Start(x[3] * power);
 }
 
 inline void MotorController::StopAll() const {
-    // NOTE: 受け取り側が6で受け取るようになってるので、配列のサイズは6にすること
-    uint8_t motorPower[6];
-    for (int i = 0; i < 6; ++i) {
-        motorPower[i] = 0b10000000;
-    }
-    Transmit_(motorPower, 6);
+    MOTOR1_.Stop();
+    MOTOR2_.Stop();
+    MOTOR3_.Stop();
+    MOTOR4_.Stop();
 }
 
 inline void MotorController::FreeAll() const {
-    // NOTE: 受け取り側が6で受け取るようになってるので、配列のサイズは6にすること
-    uint8_t motorPower[6] = {0, 0, 0, 0, 0, 0};
-    Transmit_(motorPower, 6);
+    MOTOR1_.Free();
+    MOTOR2_.Free();
+    MOTOR3_.Free();
+    MOTOR4_.Free();
 }
+
 }  // namespace component
+
 
 /* Appendix
 MotorController::Drive の 配列x の数値確認用スクリプト
