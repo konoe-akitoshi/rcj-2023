d5c2efb3814b827d471253cfbe52cbecd1fb7eb4
2357 <ryoooo2357g@gmail.com>
2023-01-09 08:37:17 +0900

    feat: you can set HIGH or LOW when you tern on LED

--- mainprogram/Program/components/led_light.hpp
+++ mainprogram/Program/components/led_light.hpp
@@ -17,8 +17,10 @@ class LedLight
   public:
     /**
      * @param pin pin number.
+     * @param on  the value(HIGH or LOW) to Write to the specified pin if you want to tern on the LED.
+     * @param off  the value(HIGH or LOW) to Write to the specified pin if you want to tern off the LED.
      */
-    explicit LedLight(SetupHandler& handler, const int pin);
+    explicit LedLight(SetupHandler& handler, const int pin, const int on = HIGH, const int off = LOW);
 
     /**
      * Tern on the LED light.
@@ -32,6 +34,8 @@ class LedLight
 
   private:
     const int PIN_;
+    const int MODE_ON_;
+    const int MODE_OFF_;
 };
 
 class LedLightPCF8574
@@ -39,8 +43,10 @@ class LedLightPCF8574
   public:
     /**
      * @param pin pin number.
+     * @param on  the value(HIGH or LOW) to Write to the specified pin if you want to tern on the LED.
+     * @param off  the value(HIGH or LOW) to Write to the specified pin if you want to tern off the LED.
      */
-    explicit LedLightPCF8574(SetupHandler& handler, Adafruit_PCF8574& pcf8574, const int pin);
+    explicit LedLightPCF8574(SetupHandler& handler, Adafruit_PCF8574& pcf8574, const int pin, const int on = HIGH, const int off = LOW);
 
     /**
      * Tern on the LED light.
@@ -55,35 +61,38 @@ class LedLightPCF8574
   private:
     Adafruit_PCF8574& pcf8574_;
     const int PIN_;
+    const int MODE_ON_;
+    const int MODE_OFF_;
 };
 
-inline LedLight::LedLight(SetupHandler& handler, const int pin) : PIN_(pin) {
+inline LedLight::LedLight(SetupHandler& handler, const int pin, const int on, const int off)
+    : PIN_(pin), MODE_ON_(on), MODE_OFF_(off) {
     handler.SetPinMode(PIN_, OUTPUT);
-    handler.SetDigitalPinDefault(PIN_, LOW);
+    handler.SetDigitalPinDefault(PIN_, MODE_OFF_);
 }
 
 inline void LedLight::TernOn() const {
-    digitalWrite(PIN_, HIGH);
+    digitalWrite(PIN_, MODE_ON_);
 }
 
 inline void LedLight::TernOff() const {
-    digitalWrite(PIN_, LOW);
+    digitalWrite(PIN_, MODE_OFF_);
 }
 
-inline LedLightPCF8574::LedLightPCF8574(SetupHandler& handler, Adafruit_PCF8574& pcf8574, const int pin)
-    : pcf8574_(pcf8574), PIN_(pin) {
+inline LedLightPCF8574::LedLightPCF8574(SetupHandler& handler, Adafruit_PCF8574& pcf8574, const int pin, const int on, const int off)
+    : pcf8574_(pcf8574), PIN_(pin), MODE_ON_(on), MODE_OFF_(off) {
     handler.SetCallback([this]() {
         pcf8574_.pinMode(PIN_, OUTPUT);
-        pcf8574_.digitalWrite(PIN_, LOW);
+        pcf8574_.digitalWrite(PIN_, MODE_OFF_);
     });
 }
 
 inline void LedLightPCF8574::TernOn() const {
-    pcf8574_.digitalWrite(PIN_, HIGH);
+    pcf8574_.digitalWrite(PIN_, MODE_ON_);
 }
 
 inline void LedLightPCF8574::TernOff() const {
-    pcf8574_.digitalWrite(PIN_, LOW);
+    pcf8574_.digitalWrite(PIN_, MODE_OFF_);
 }
 }  // namespace component
 
