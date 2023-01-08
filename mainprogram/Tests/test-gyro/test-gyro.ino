88a232835343681639e5f08c695973d649900f2a
2357 <ryoooo2357g@gmail.com>
2023-01-09 08:17:48 +0900

    test: `component::Gyro`

---
+++ mainprogram/Tests/test-gyro/test-gyro.ino
@@ -0,0 +1,16 @@
+#include "components/setup_handler.hpp"
+#include "components/gyro.hpp"
+
+component::SetupHandler Handler;
+const component::Gyro Gyro(Handler, 115200);
+
+void setup() {
+    Serial.begin(9600);
+    Handler.Setup();
+}
+
+void loop() {
+    const auto rot = Gyro.GetRotation();
+    Serial.println(rot);
+    delay(100);
+}
