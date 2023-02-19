c14d44f0d873d783f6f2f53ded7135f6c8b6734e
sepa38 <82813488+sepa38@users.noreply.github.com>
2023-02-19 22:33:51 +0900

    fix: I2C test python program

--- Vison-M1n/I2C.py
+++ Vison-M1n/I2C.py
@@ -1,35 +1,22 @@
-#I2C通信テスト
 
 from machine import I2C
-import time
+import utime
 
 count = 0
 
+def on_receive (data):
+    print ("on_receive:", data)
 
-def on_receive(data):
-    print("on_receive:", data)
-
-
-def on_transmit():
+def on_transmit ():
     global count
     count = count + 1
-    print("on_transmit, send:", count)
-    return "12345"[:(count) % 5 + 1]
+    print ("on_transmit, send:", count)
+    return count
 
+def on_event (event):
+    print ("on_event:", event)
 
-def on_event(event):
-    print("on_event:", event)
+i2c = I2C (I2C.I2C0, mode = I2C.MODE_SLAVE, scl = 20, sda = 21, addr = 0x24, addr_size = 7, on_receive = on_receive, on_transmit = on_transmit, on_event = on_event)
 
-
-i2c = I2C(I2C.I2C0,
-          mode=I2C.MODE_SLAVE,
-          scl=20,
-          sda=21,
-          addr=0x24,
-          addr_size=7,
-          on_receive=on_receive,
-          on_transmit=on_transmit,
-          on_event=on_event)
-
-while True:
-    time.sleep(1)
+while(True):
+    utime.sleep_ms(100)
