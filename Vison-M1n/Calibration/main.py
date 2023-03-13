470aa6e8c9eb11cc0d62e2eaaefee05f80f63089
2357 <ryoooo2357g@gmail.com>
2023-03-13 21:40:42 +0900

    refoactor: use main() and add comment to calibration  program

--- Vison-M1n/Calibration/main.py
+++ Vison-M1n/Calibration/main.py
@@ -5,7 +5,6 @@ import glob
 import shutil
 
 # サークルグリットの点の数
-# CIRCLEGRID = (6,9)
 CIRCLEGRID = (12, 8)
 
 # cv2.TERM_CRITERIA_EPS
@@ -15,6 +14,80 @@ CIRCLEGRID = (12, 8)
 # cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER
 #    上記のどちらかの条件が満たされた時に繰り返し計算を終了する
 CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+
+def main():
+    clean("./undistorted/")
+    clean("./undistorted_hand/")
+    clean("./corners/")
+
+    targets = Targets("./images/*.jpg")
+    h, w = targets.size()
+    objectPoints, imagePoints = extractPaths(targets)
+    status, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objectPoints,
+        imagePoints,
+        (w, h),
+        None,
+        None
+    )
+
+    # キャリブレーションによって得られたパラメータを使用してカメラ行列を改良する
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
+        mtx,     # カメラ行列
+        dist,    # 歪み係数のベクトル
+        (w, h),  # 元の画像のサイズ
+        1,       # 歪み修正後の画像にできるの隙間をどうするか (0 ~ 1)
+                 #   0: zoomする、1: 黒で塗りつぶす
+        (w, h)   # 歪み修正後の画像サイズ (省略すると第3引数と同じ値になる)
+    )
+
+    print("Camera matrix:")
+    print(mtx)
+    print("New camera matrix:")
+    print(newcameramtx)
+    print("dist:")
+    print(dist)
+    print("rvecs:")
+    print(rvecs)
+    print("tvecs:")
+    print(tvecs)
+
+    mapx, mapy = cv2.initUndistortRectifyMap(
+        mtx,           # カメラ行列
+        dist,          # 歪み係数のベクトル
+        None,          # 空間の回転行列(3*3)
+        newcameramtx,  # 補正後の画像のスケーリングや移動を行えるようの別のカメラ行列
+                       #   省略すると、第2引数と同じ値になる
+        (w, h),        # 歪み補正後の画像サイズ
+        5              # = CV_32FC1
+    )
+
+    hand_mapx = np.zeros((w, h), dtype=np.float32)
+    hand_mapy = np.zeros((w, h), dtype=np.float32)
+    cx, cy = mtx[0][2], mtx[1][2]
+    fx, fy = mtx[0][0], mtx[1][1]
+    cx2, cy2 = newcameramtx[0][2], newcameramtx[1][2]
+    fx2, fy2 = newcameramtx[0][0], newcameramtx[1][1]
+    k1, k2, p1, p2, k3 = dist[0]
+    for u in range(w):
+        for v in range(h):
+            x = (u - cx2) / fx2
+            y = (v - cy2) / fy2
+            rr = x ** 2 + y ** 2
+            K = 1 + k1*rr + k2*(rr**2) + k3*(rr**3)
+
+            x2 = x*K + 2*p1*x*y + p2*(rr+2*(x**2))
+            y2 = y*K + p1*(rr+2*(y**2)) + 2*p2*x*y
+            hand_mapx[u][v] = x2*fx + cx
+            hand_mapy[u][v] = y2*fy + cy
+
+    for image in targets.images:
+        dst = cv2.remap(image.image, mapx, mapy, cv2.INTER_LINEAR)
+        cv2.imwrite("./undistorted/" + image.name, dst)
+
+        dst = cv2.remap(image.image, hand_mapx, hand_mapy, cv2.INTER_LINEAR)
+        cv2.imwrite("./undistorted_hand/" + image.name, dst)
 
 
 class ImageData:
@@ -35,6 +108,9 @@ class Targets:
             self.images.append(ImageData(fname))
 
     def size(self):
+        """
+        Returns: (h, w)
+        """
         ret_h = self.images[0].h
         ret_w = self.images[0].w
         for img in self.images:
@@ -51,31 +127,31 @@ def clean(path):
 
 
 def extractPaths(targets):
-    # Defining the world coordinates for 3D points
+    # ワールド座標系におけるキャリブレーションボードの各点の座標
     objp = np.zeros((1, CIRCLEGRID[0]*CIRCLEGRID[1], 3), np.float32)
     objp[0, :, :2] = np.mgrid[0:CIRCLEGRID[0],
                               0:CIRCLEGRID[1]].T.reshape(-1, 2)
 
-    # Vectors of 3D points for each CIRCLEGRID image
-    objpoints = []
-
-    # Vectors of 2D points for each CIRCLEGRID image
-    imgpoints = []
+    # キャリブレーションで利用する座標
+    objpoints = []  # Vectors of 3D points for each CIRCLEGRID image
+    imgpoints = []  # Vectors of 2D points for each CIRCLEGRID image
 
     for image in targets.images:
         print(image.name)
+
+        # グレースケールを使って、キャリブレーションボード内の点の座標を取得
         gray = cv2.cvtColor(image.image, cv2.COLOR_BGR2GRAY)
-        # Find the chess board corners
         found, corners = cv2.findCirclesGrid(
             gray,
             CIRCLEGRID,
             cv2.CALIB_CB_SYMMETRIC_GRID
         )
+
         if found:
             print("found:", image.name)
             objpoints.append(objp)
 
-            # refining pixel coordinates for given 2d points.
+            # 座標の精度を上げる
             corners2 = cv2.cornerSubPix(
                 gray,
                 corners,
@@ -85,64 +161,16 @@ def extractPaths(targets):
             )
             imgpoints.append(corners2)
 
-            # Draw and display the corners
-            corners3 = cv2.drawChessboardCorners(
+            # コーナーが描かれた画像を生成
+            corners_img = cv2.drawChessboardCorners(
                 image.image.copy(),
                 CIRCLEGRID,
                 corners2,
                 True
             )
-            cv2.imwrite('./corners/' + image.name, corners3)
+            cv2.imwrite("./corners/" + image.name, corners_img)
     return (objpoints, imgpoints)
 
 
 if __name__ == "__main__":
-    clean('./undistorted/')
-    clean('./corners/')
-
-    targets = Targets('./image/*.jpg')
-    h, w = targets.size()
-
-    objectPoints, imagePoints = extractPaths(targets)
-    status, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-        objectPoints,
-        imagePoints,
-        (w, h),
-        None,
-        None
-    )
-
-    print("Camera matrix :")
-    print(mtx)
-    print("dist :")
-    print(dist)
-    print("rvecs :")
-    print(rvecs)
-    print("tvecs :")
-    print(tvecs)
-
-    for image in targets.images:
-        # Refining the camera matrix using parameters obtained by calibration
-        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
-            mtx,
-            dist,
-            (w, h),
-            1,
-            (w, h)
-        )
-
-        # Method 1 to undistort the image
-        dst = cv2.undistort(image.image, mtx, dist, None, newcameramtx)
-
-        # Method 2 to undistort the image
-        mapx, mapy = cv2.initUndistortRectifyMap(
-            mtx,
-            dist,
-            None,
-            newcameramtx,
-            (w, h),
-            5
-        )
-
-        dst = cv2.remap(image.image, mapx, mapy, cv2.INTER_LINEAR)
-        cv2.imwrite('./undistorted/' + image.name, dst)
+    main()
